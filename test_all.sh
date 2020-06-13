af7cd6de0e422a29f9b8287120db1dd013daf330
clewsy <jadon@clews.pro>
2020-06-13 21:17:15 +1000

    Modified common/common_scripts/bu.sh section - backup file lists now a variable in host_vars files.

---
+++ test_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ansible-playbook b4t-cam.yml &&
+ansible-playbook b4t.site.yml &&
+ansible-playbook calculon.yml &&
+ansible-playbook farnsworth.yml &&
+ansible-playbook hypnotoad.yml &&
+#ansible-playbook nibbler.yml &&
+ansible-playbook p0wer.yml &&
+ansible-playbook pazuzu.yml &&
+ansible-playbook rad10.yml &&
+ansible-playbook seymour.yml &&
+ansible-playbook zapp.yml &&
+ansible-playbook zoidberg.yml

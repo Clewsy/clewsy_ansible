db8c5c3e30ec18a8259eeafc32e5c9babbdb3d37
clewsy <jadon@clews.pro>
2021-09-19 11:01:07 +1000

    Some hosts offline for now.

--- test_all.sh
+++ test_all.sh
@@ -3,14 +3,14 @@
 ansible-playbook playbooks/b4t-cam.yml		&&
 ansible-playbook playbooks/b4t.site.yml		&&
 ansible-playbook playbooks/calculon.yml		&&
-ansible-playbook playbooks/farnsworth.yml	&&
+#ansible-playbook playbooks/farnsworth.yml	&&
 #ansible-playbook playbooks/flexo.yml		&&
 ansible-playbook playbooks/hermes.yml	        &&
 ansible-playbook playbooks/hypnotoad.yml        &&
 #ansible-playbook playbooks/nibbler.yml		&&
-ansible-playbook playbooks/p0wer.yml		&&
-ansible-playbook playbooks/pazuzu.yml		&&
-ansible-playbook playbooks/rad10.yml		&&
+#ansible-playbook playbooks/p0wer.yml		&&
+#ansible-playbook playbooks/pazuzu.yml		&&
+#ansible-playbook playbooks/rad10.yml		&&
 ansible-playbook playbooks/seymour.yml		&&
 ansible-playbook playbooks/zapp.yml	    	&&
 ansible-playbook playbooks/zoidberg.yml

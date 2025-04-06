82ec85eac9c32da39453148ab27da5f30ff6c207
clewsy <jadon@clews.tech>
2025-04-06 15:44:59 +1000

    Auto backup script improvements.

--- roles/rsync_server/templates/rsync_server_backup_script.sh
+++ roles/rsync_server/templates/rsync_server_backup_script.sh
@@ -9,11 +9,22 @@ SYNC_LOGFILE={{ rsync_server_logfile }}
 # Check for sudo.
 if [[ "${EUID}" != 0 ]]; then printf "%b" "Must be run as root.  Quitting\n" && exit 1; fi
 
+# Log header.
 printf "%b" "---------------------------------------------------------\n" >> ${SYNC_LOGFILE}
 printf "%b" "---------- Job Name : ${SYNC_NAME}\n" >> ${SYNC_LOGFILE}
 printf "%b" "---------- Job Started : $(date +%Y-%m-%d\ %T)\n" >> ${SYNC_LOGFILE}
 
-rsync --verbose --archive --delete --acls --human-readable --progress --log-file=${SYNC_LOGFILE} -e "ssh -o StrictHostKeyChecking=no" ${SYNC_SOURCE} ${SYNC_DEST}
+rsync   --verbose \
+        --archive \
+        --delete \
+        --acls \
+        --human-readable \
+        --progress \
+        --log-file=${SYNC_LOGFILE} \
+        --rsync-path="sudo rsync" \
+        -e "ssh -o StrictHostKeyChecking=no" \
+        ${SYNC_SOURCE} ${SYNC_DEST}
 
+# Log tail.
 printf "%b" "---------- Job Completed : $(date +%Y-%m-%d\ %T)\n" >> ${SYNC_LOGFILE}
 printf "%b" "---------------------------------------------------------\n" >> ${SYNC_LOGFILE}

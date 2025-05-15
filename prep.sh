b5b25d1f4eb5547956061100388a08307b49d700
clewsy <jadon@clews.tech>
2025-05-15 13:51:37 +1000

    Cleanup.

--- prep.sh
+++ prep.sh
@@ -2,6 +2,6 @@
 # Install some software required by ansible for first run.
 
 sudo apt update
-sudo apt install	openssh-server \
-			sshpass \
-			ansible
+sudo apt install    openssh-server \
+                    sshpass \
+                    ansible
